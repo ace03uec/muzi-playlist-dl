37c3c057d5d651c0d919fac87027cbade661f110
Giridaran M <giridar123@gmail.com>
2014-04-02 12:20:55 +0530

    Version 0.1, Untested

--- muziPlaylist-dl.py
+++ muziPlaylist-dl.py
@@ -8,6 +8,8 @@ import json
 muzi_url = 'https://muzi.sdslabs.co.in/'
 cdn_url = 'https://music.sdslabs.co.in/'
 playlists_url = 'https://muzi.sdslabs.co.in/ajax/playlist/list.php'
+tracks_url = 'https://muzi.sdslabs.co.in/ajax/track/?id='
+default_path = os.environ["HOME"]+"/muziPlaylists/"
 
 #Extract Playlists available
 avlbl_playlists = urllib2.urlopen(playlists_url)
@@ -20,12 +22,35 @@ for playlist in avlbl_playlists_json:
 
 #Playlist Choice and JSON extract
 choice = raw_input('Choose which playlist to download. [1,2,3,...]')
-choice = choice -1
+choice = int(choice)-1
 chosen_playlist = avlbl_playlists_json[choice]
 chosen_playlist_id = chosen_playlist['id']
 chosen_playlist_url = muzi_url+"ajax/playlist/?id="+chosen_playlist_id
 chosen_playlist_json = json.load(urllib2.urlopen(chosen_playlist_url))
 
-#Playlist download
+#Playlist Info
+chosen_playlist_tracks = chosen_playlist_json['tracks']
+chosen_playlist_creator = chosen_playlist_json['username']
+chosen_playlist_name = chosen_playlist_json['name']
 
-    
+#Create folders
+os.mkdir(default_path,0755)
+chosen_playlist_folder = default_path+chosen_playlist_name
+os.mkdir(chosen_playlist_folder,0755) 
+
+#Download Tracks
+for track in chosen_playlist_tracks:
+	file_name = track['title']+'-'+track['artist']+'.mp3'
+	file_url_json = json.load(urllib2.urlopen(tracks_url+track['id']))
+	file_suburl = file_url_json['file']
+	temp_file_url = cdn_url+file_suburl
+	temp_file_url = temp_file_url.replace('\\','/')
+	final_file_url = temp_file_url.replace(' ','%20')
+	mp3_file = urllib2.urlopen(final_file_url)
+	with open(chosen_playlist_folder+file_name,'wb') as tune:
+		tune.write(mp3_file.read())
+
+
+		
+	
+ 
