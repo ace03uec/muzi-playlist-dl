55391b790578756c9d02630f8d8cc760001ce8be
Giridaran M <giridar123@gmail.com>
2014-04-02 10:33:19 +0530

    added upto playlist choice

--- muziPlaylist-dl.py
+++ muziPlaylist-dl.py
@@ -4,13 +4,28 @@ import urllib2
 import errno
 import json
 
+#Base URL's required
 muzi_url = 'https://muzi.sdslabs.co.in/'
 cdn_url = 'https://music.sdslabs.co.in/'
-playlist_url = 'https://muzi.sdslabs.co.in/ajax/playlist/list.php'
+playlists_url = 'https://muzi.sdslabs.co.in/ajax/playlist/list.php'
 
-avlbl_playlists = urllib2.urlopen(playlist_url)
+#Extract Playlists available
+avlbl_playlists = urllib2.urlopen(playlists_url)
 avlbl_playlists_json = json.load(avlbl_playlists)
 print 'available playlists: \n'
-for x in avlbl_playlists_json:
-	print x['name']
+index = 0
+for playlist in avlbl_playlists_json:
+    index = index+1
+    print str(index)+"\t"+playlist['name']+"\t"+playlist['username']
 
+#Playlist Choice and JSON extract
+choice = raw_input('Choose which playlist to download. [1,2,3,...]')
+choice = choice -1
+chosen_playlist = avlbl_playlists_json[choice]
+chosen_playlist_id = chosen_playlist['id']
+chosen_playlist_url = muzi_url+"ajax/playlist/?id="+chosen_playlist_id
+chosen_playlist_json = json.load(urllib2.urlopen(chosen_playlist_url))
+
+#Playlist download
+
+    
